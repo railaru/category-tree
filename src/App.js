8565a8f729a66ba2c735e3c4fded5860b37ff419
Rutenis Raila <rutenisraila@hotmail.com>
2019-12-12 16:18:16 +0000

    moved to repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { treeData } from './tree-data';
+
+const flattenNestedObject = data => {
+  return data.reduce((result, next) => {
+    result.push(next);
+    if (next.children) {
+      result = result.concat(flattenNestedObject(next.children));
+      next.children = [];
+    }
+    return result;
+  }, []);
+};
+
 function App() {
+  const flatData = flattenNestedObject(treeData);
+  const [data, setData] = useState(flatData);
+  const [value, setValue] = useState('');
+  const [hierarchy, setHierarchy] = useState('');
+
+  const addNew = e => {
+    e.preventDefault();
+    const newValue = {
+      id: data.length,
+      value,
+      hierarchy
+    };
+    if (value && hierarchy) {
+      setData(data => [...data, newValue]);
+    } else {
+      alert('please fill the form!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="category-form">
+        <label htmlFor="category-value">Category value</label>
+        <input
+          type="text"
+          name="Category value"
+          id="category-value"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        <label htmlFor="category-hierarchy">Category hierarcy</label>
+        <input
+          type="number"
+          name="Hierarchy value"
+          id="category-hierarchy"
+          value={hierarchy}
+          onChange={e => setHierarchy(e.target.value)}
+        />
+        <button onClick={e => addNew(e)}>Add</button>
+      </form>
+      <ul>
+        {data.map(category => (
+          <li key={category.id} style={{ marginLeft: category.hierarchy * 10 }}>
+            {category.value}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/tree-data.js
@@ -0,0 +1,16 @@
+export const treeData = [
+  {
+    id: 0,
+    hierarchy: 1,
+    value: 'Category 1',
+    children: [
+      { id: 1, hierarchy: 2, value: 'Category 2', children: [] },
+      {
+        id: 2,
+        hierarchy: 2,
+        value: 'Category 3',
+        children: [{ id: 3, hierarchy: 3, value: 'Category 4', children: [] }]
+      }
+    ]
+  }
+];
